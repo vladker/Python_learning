5ab7d343719f50033696bab6e6b936af5b1f18f9
Vlad Rogachev <79264016668@yandex.ru>
2022-01-29 22:14:48 +0300

    7 Euler task is solved, tried to lighten algorithm by including "//" in first after division, cause number after division by itself can be divied only by half of this number, so algorythm exclud range (n//2:n-1) if is nearly half of calculation, and the other lightener is breaking while if number has already 3 divider, and is not simple obvious.

--- Euler Project/Euler 7.py
+++ Euler Project/Euler 7.py
@@ -3,4 +3,27 @@
 # Выписав первые шесть простых чисел, получим 2, 3, 5, 7, 11 и 13. Очевидно, что 6-е простое число - 13.
 #
 # Какое число является 10001-м простым числом?
+divided= 1
+divider_total = 0
+while divided>0:
+    divider = divided
+    divider_count = 0
+    step = 1
+    while divider>0:
+        if divided % divider == 0:
+            divider_count += 1
+        if step == 1:
+            divider = divider // 2
+            step += 1
+        else:
+            divider -= 1
+        if divider_count == 3:
+            break
+    if divider_count == 2:
+        divider_total += 1
+    if divider_total == 10001:
+        print("10001 simple divider is", divided)
+        break
+    divided+=1
+
 
