6f3d01191a0e01650d732453d89465374cbcc9aa
Vlad Rogachev <79264016668@yandex.ru>
2022-01-29 22:16:34 +0300

    Duplicate remover is very simple but not effective program which takes onle dictionary with words in rows and create new file with only unsorted unique words. Algo is not optimal, it's my first py with files.

--- Pandas/duplicate remover.py
+++ Pandas/duplicate remover.py
@@ -3,9 +3,10 @@ file_source = open("C:/Users/rogac/Downloads/DICS/totality.dic", mode="r", encod
 line_set=set()
 for line_base in file_source:
     line_set.add(line_base)
+    print(len(line_set))
 file_source.close()
 
-file_target = open("C:/Users/rogac/Downloads/DICS/totality2.dic", mode="a+", encoding="latin-1")
+file_target = open("C:/Users/rogac/Downloads/DICS/totality4.dic", mode="a+", encoding="latin-1")
 for elements in line_set:
     file_target.write(elements)
 file_target.close()
