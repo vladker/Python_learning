418b02d2538568b3f5c1a608a921ec33fc2225af
Vlad Rogachev <79264016668@yandex.ru>
2022-02-26 11:45:43 +0300

    Added training plot

--- AI/ADALINE.py
+++ AI/ADALINE.py
@@ -32,3 +32,61 @@ plt.xlabel('Lenght cup')
 plt.ylabel('Lenght wing')
 plt.legend(loc = 'upper left')
 plt.show()
+
+# Description of perceptron
+class Perceptron(object):
+    '''
+    eta:float - speed
+    n_iter:int - rounds
+    w_:1-axis array, weight after adjustment
+    errors_: list of errors of classification
+    '''
+
+    def __init__(self, eta = 0.01, n_iter = 10):
+        self.eta = eta
+        self.n_iter = n_iter
+
+    '''
+    Adjust model to learning array
+    X: array with format = [n_samples, n_features] training vectors
+    n_samples - examples
+    n_features - features
+    
+    y: array with format = [n_samples] target values
+    returns 
+    self: object
+    '''
+
+    def fit(self, x, y):
+        self.w_ = np.zeros(1+X.shape[1])
+        self.errors_ = []
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X,y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update * xi
+                self.w_[0] += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+        return self
+
+    '''
+    Evaluate clean input
+    '''
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:] + self.w_[0])
+
+    '''Return class marker after single jump'''
+    def predict(self, X):
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
+
+'''Training'''
+ppn = Perceptron(eta = 0.1, n_iter = 10)
+ppn.fit(X,y)
+plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker = 'o')
+plt.xlabel('Epoches')
+
+'''Errors of classification'''
+plt.ylabel('Errors of classification')
+plt.show()
+
