569a72b9164acc21dbdd31b1b399de39390c96ce
Vlad Rogachev <79264016668@yandex.ru>
2022-03-10 00:46:40 +0300

    New layers and less hidden

--- AI/Mature training.py
+++ AI/Mature training.py
@@ -8,9 +8,10 @@ from pybrain3.datasets import SupervisedDataSet
 from  pybrain3.supervised.trainers import BackpropTrainer
 from pybrain3.tools.xml.networkreader import NetworkReader
 from pybrain3.tools.xml.networkwriter import NetworkWriter
+from pybrain3.structure import SoftmaxLayer
+from pybrain3.structure import TanhLayer
 
 # Create a dataset for training
-
 ds = SupervisedDataSet(15, 1)
 with open('training mature.csv', newline='') as csvfile:
     data_csv = csv.reader(csvfile, delimiter=';')
@@ -22,16 +23,11 @@ with open('training mature.csv', newline='') as csvfile:
         target = float(row[-1].replace(",","."))
         ds.addSample(input,target)
 
-        # print(row[:-1])
-        # print(row[-1])
-        # print(', '.join(row))
-
-
 # Create structure of network
-net = buildNetwork(15, 3, 1, bias=True)
+net = buildNetwork(15, 3, 1, hiddenclass=TanhLayer, outclass=SoftmaxLayer)
+
 
 # Network training with visualization
-
 trainer = BackpropTrainer(net, dataset=ds, momentum=0.1, learningrate=0.01, verbose=True, weightdecay=0.01)
 trnerr, valerr = trainer.trainUntilConvergence()
 plt.plot(trnerr, 'b', valerr, 'r')
